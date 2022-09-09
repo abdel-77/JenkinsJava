7e837d3401366dcc7b43e28d38d14f20916d646f
abdel.bessaa <abdel.bessaa@free.fr>
2022-09-09 17:01:36 +0200

    jenkinsfile

--- Jenkinsfile.js
+++ Jenkinsfile.js
@@ -10,7 +10,7 @@ pipeline {
         stage('Clone') {
             steps {
                 checkout([$class: 'GitSCM',
-                    branches: [[name: '*/master' ]],
+                    branches: [[name: '*/main' ]],
                     extensions: scm.extensions,
                     userRemoteConfigs: [[
                         url: 'https://github.com/abdel-77/JenkinsJava.git',
